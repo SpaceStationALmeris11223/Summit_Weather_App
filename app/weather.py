cce16a36ed72c146ec5549e645302f5883a7df00
SpaceStationALmeris11223 <125327160+SpaceStationALmeris11223@users.noreply.github.com>
2025-04-18 05:27:55 +0000

    Don't merge with main. I haven't tested it yet to see if it runs. Feel free to make changes and run it.

---
+++ app/weather.py
@@ -0,0 +1,16 @@
+import requests
+#↑allows use of request library
+import os
+#used to access environmental variables(secret keys, config settings, etc.)
+#these variables come from .env file so private info ist put into the code
+weather_API = os.getenv("Weather_API_Key")
+#looks into the .env file for the api key and saves it to a python variable named weather api
+
+#function that gets the weather
+def get_weather(location):
+    url = #need to finish this part the api key is P2KgqKXqCM9UZ2foof32w4bOLzPhVNjA9M9le5hP
+    #insert the api key with the url
+    response = requests.get(url)
+    #↑stores api's respoonse in 'response'
+    return response.json()
+#→→→→→↑takes the raw jsaon file from the api and converts it into data
