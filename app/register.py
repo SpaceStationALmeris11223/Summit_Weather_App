8b81a56e6c97d063f084e00cb80aba3e8c56499b
SpaceStationALmeris11223 <125327160+SpaceStationALmeris11223@users.noreply.github.com>
2025-05-09 23:48:50 +0000

    Register works just need to connect it to a database to check and see if login works as well.

--- app/register.py
+++ app/register.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, url_for, render_template, flash
+from flask import request, redirect, url_for, render_template, flash,session
 from werkzeug.security import generate_password_hash
 from .models  import db, User
 from markupsafe import escape
@@ -28,29 +28,61 @@ def valid_username(username):
 
 def valid_password(password):
     #validate password input
-        if (len(password) < 8 or
-            not re.search(r'[a-zA-Z]', password) or
-            not re.search(r'\d', password) or
-            not re.search(r'[!@#$%^&*(),.?":{}|<>]', password)
-        ):
-            flash("Password must be 8+characters, includes letters, numbers,and symbols.")
+
+        if len(password) < 8:
+            flash("Password must be at least 8 characters")
+            return False
+        if not re.search(r'[a-zA-Z]', password):
+            flash("Password must include at least one letter.")
+            return False
+        if not re.search(r'\d', password):
+            flash("Password must include at least one number")
+            return False
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            flash("Password must contain at least one special character")
+            return False
+        return True
+
+ 
+
+@main.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method =="POST":    
+        #cleans up the form inputs↓
+        username = escape(request.form.get("username", "").strip())
+        password = request.form.get("password", "").strip()     
+
+
+            #validate inputs
+        username_invalid = valid_username(username)
+        password_invalid = valid_password(password)
+        #username check
+        if username_invalid:
+                for error in username_invalid:
+                    flash(error)
+                return redirect(url_for("main.register"))
+        #password check
+        if password_invalid:
+            for error in password_invalid:
+                    flash(error)
             return redirect(url_for("main.register"))
-        
-    #profanity check
-        if profanity.contains_profanity(username) or profanity.contains_profanity(password):
-            flash("Username or password uses inappropriate language")
+            #if username exist
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash("Username already exist.")
             return redirect(url_for("main.register"))
 
-    #store user using set_passoword
+        #create new user
         new_user = User(username=username)
         new_user.set_password(password)
         try:
             db.session.add(new_user)
             db.session.commit()
-            flash("Account created")
+            session["user_id"] = new_user.id #auto login
+            flash("Account created!")
             return redirect(url_for("main.home"))
-        except:
+        except Exception as e:
             db.session.rollback()
-            flash("Username already exist or an error occurred.")
+            flash("error occurred during registration.")
             return redirect(url_for("main.register"))
     return render_template("register.html")
