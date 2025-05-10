85995d6aff3374bb64edf9b404c8d9960b422b73
SpaceStationALmeris11223 <125327160+SpaceStationALmeris11223@users.noreply.github.com>
2025-05-10 03:03:17 +0000

    should work with websters database needs to be checked tho

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,21 @@
 #Thsi files will pull flask and other relavant tools
 from flask import Flask
-from .routes import main
-# ↓ allows the app to use a database
-from flask_sqlalchemy import SQLAlchemy
+
 from dotenv import load_dotenv
 #↑ Allows the app to read hidden values from a .env file(Api Keys, passwords, etc.)
 import os
 #↑ Allows the app to interact with your operating system
 
 #A circular import was created so i made an extensions files to solve it 
-from .extensions import db 
+from .extensions import db # db = SQLAlchemy resides in here
 #↑ creates a database object
 def create_app():
     load_dotenv()
 #looks for ↑ file called .env adn loads any key value pairs
     app = Flask(__name__)
+
+
+
 
 
 
@@ -25,12 +26,13 @@ def create_app():
     app.config['WEATHER_API_URL'] = os.environ.get('WEATHER_API_URL')
     app.config['WEATHER_API_KEY'] = os.environ.get('Weather_API_Key')
     #setting up sqlalchemy↓
+    basedir = os.path.abspath(os.path.dirname(__file__))
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///weather.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #The connects ↓ the db to this app
     db.init_app(app)#←connects app to database
-#Register blueprints routes↓
+#Register blueprints routes↓ app needs to initialize first
     from .routes import main
 #    ↑imports the routes Aka different pages(urls)
 #   ↓plugs in (connects) the recently imported routes to the app
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from app import create_app
-from flask import Flask, Blueprint
-# ↑Loads the app code
-app = create_app()
 
+
+app = create_app()
 
 if __name__ == "__main__":
 # ↑ only runs if it's the main file
